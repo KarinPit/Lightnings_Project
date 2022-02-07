72057030a8f244f458bcdb4208c270217e247312
KarinPit <99114944+KarinPit@users.noreply.github.com>
2022-02-07 20:53:01 +0200

    added a way to loop over the date in the time array of the data. This allows to get the plots for each date, but still not the data array

--- Lightning_intens_with_alk.py
+++ Lightning_intens_with_alk.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -6,10 +7,20 @@ import pandas as pd
 sal_path = 'D:/WWLLN-Intensity/Validation CSV/sal.nc'
 sal_ds = xr.open_dataset(sal_path)
 sal_array = sal_ds.so.isel(time=100)
+time_array = sal_ds['time'].data
 
-temp_path = 'D:/WWLLN-Intensity/Validation CSV/ptemp.nc'
-temp_ds = xr.open_dataset(temp_path)
-temp_array = temp_ds.thetao.isel(time=50)
+for i in time_array:
+    date = np.datetime64(i, 'M')
+    if date == np.datetime64('2009-12'):
+        time_list = time_array.tolist()
+        i = int(i)
+        index_i = time_list.index(i)
+        sal_array = sal_ds.so.isel(time=index_i)
+
+
+# temp_path = 'D:/WWLLN-Intensity/Validation CSV/ptemp.nc'
+# temp_ds = xr.open_dataset(temp_path)
+# temp_array = temp_ds.thetao.isel(time=50)
 
 # temp_array.plot()
 # plt.show()
