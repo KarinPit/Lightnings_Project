c126b7896b36738a3284a6e8c5f75d4cc4c85f7b
KarinPit <99114944+KarinPit@users.noreply.github.com>
2022-02-08 21:42:56 +0200

    added code to calculate mean alkalinty for 2009-2020 dec,jan, feb

--- Lightning_intens_with_alk.py
+++ Lightning_intens_with_alk.py
@@ -1,21 +1,80 @@
 import xarray as xr
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 nc_file = 'D:/WWLLN-Intensity/Validation CSV/alk.nc'
 alk_ds = xr.open_dataset(nc_file)
-alk_sample = alk_ds.talk.isel(time=0)
-# notice that by specifying longitude = 1 there is no need to create a list
-alk_splong = alk_sample.isel(longitude=1)
-lat_array = alk_splong.latitude.data.tolist()
-alk_splong_data = alk_splong.data.tolist()[0]
+times = alk_ds.time.to_pandas()
+pandas_times = alk_ds.time.to_pandas()
+array_list = []
+for i in pandas_times:
+    file_name = str(i.year) + '-' + str(i.month)
+    if i.month in [1, 2, 3]:
+        ptimes_list = pandas_times.to_list()
+        index = ptimes_list.index(i)
+        alk_stime = alk_ds.talk.isel(time=index)
+        np_arr = alk_stime.to_numpy()
+        np_arr = np.nan_to_num(np_arr, nan=0)
+        df = pd.DataFrame(np_arr[0])
 
+        df.to_csv(f'D:/WWLLN-Intensity/Validation CSV/{file_name}.csv')
+        array_list.append(np_arr)
+mean_array = np.mean(array_list, axis=0)
+df = pd.DataFrame(mean_array[0])
+df.to_csv(f'D:/WWLLN-Intensity/Validation CSV/mean.csv')
 
-plt.plot(lat_array, alk_splong_data, marker='o', color='blue')
+lat_list = alk_ds.latitude.data.tolist()
+long_list = alk_ds.longitude.data.tolist()
+cmap = cm.get_cmap('rainbow')
+cmap.set_under('w')
+plt.pcolormesh(long_list, lat_list, mean_array[0], cmap= cmap, shading='gouraud')
+plt.clim(2.4, 2.8)
+cb = plt.colorbar()
+cb.set_label('Mol * m^-3', fontsize=14, rotation=90, labelpad=30)
 plt.show()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# nc_file = 'D:/WWLLN-Intensity/Validation CSV/alk.nc'
+# alk_ds = xr.open_dataset(nc_file)
+# times = alk_ds.time.to_pandas()
+# months = ['1', '2', '3']
+#
+# for i in times:
+#     if i in months:
+#         alk_sample = alk_ds.talk.isel(time=i)
+#
+# alk_sample = alk_ds.talk.isel(time=0)
+# # notice that by specifying longitude = 1 there is no need to create a list
+# alk_splong = alk_sample.isel(longitude=1)
+# lat_array = alk_splong.latitude.data.tolist()
+# alk_splong_data = alk_splong.data.tolist()[0]
+# plt.plot(lat_array, alk_splong_data, marker='o', color='blue')
+# # plt.show()
+#
+#
 
 
 
